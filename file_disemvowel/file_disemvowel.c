46ef3f48f5999cf65c336d3499d5d6ea4563c44d
nathan-beneke <benek020@morris.umn.edu>
2017-10-03 12:14:37 -0500

    Create methods to be consistent with structure of solution Change argument handling to be correct

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,26 +1,40 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define BUF_SIZE 1024
+
+bool is_vowel(char c){
+
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char*out_buf){
+
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile){
+
+}
 
 int main(int argc, char *argv[]){
 	FILE *inputFile;
 	FILE *outputFile;
 	
+	if(argc != 0 && argc != 2){
+		printf("Ya done goofed");
+		return 1;
+	}
+
 	if(argc == 0){
 		//grabs from command line
 
 	}
 
 	if(argc == 2){
-		inputFile = argv[0];
-		outputFile = argv[1];
+		inputFile = fopen(argv[1], "r");
+		outputFile = fopen(argv[2], "w");
 	}
 
-	if(argc != 0 && argc != 2){
-		printf("Ya done goofed");
-		return 1;
-	}
-
+	disemvowel(inputFile, outputFile);
 
 	return 0;
 }
